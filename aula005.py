9b162dee5d33ad88f5c297e1ed5f1fd7bbff2f95
Daniel Douglas de Souza <120666117+Daniel-Douglas-de-Souza@users.noreply.github.com>
2022-12-16 18:41:19 -0300

    Adiciona aula de variáveis em Python
    Variáveis que ocupam espaço na memória

---
+++ aula005.py
@@ -0,0 +1,26 @@
+"""""
+Variáveis, são espaços reservados na memória do computador
+que criamos para guardar dados para ser utilizados
+durante a execução do programa.
+"""
+x = 5
+y = " Daniel"
+z = 4 # é de tipo int
+z = "Logan" # agora é do tipo string
+
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+
+a = str(3) # a será "3"
+b = int(3) # b será 3
+c = float(3) # c será 3.0 número real, que aceita casas decimais
+
+# 3  Número inteiro
+# "3"  Um caracter ou string
+#3.0  Um número real
+
+d = 4
+D = "Danny" # embora seja a nesna letra, porém como em python ele é um case sensitive, ele diferencia 
+            # entre Maiúscula e Minúscula
